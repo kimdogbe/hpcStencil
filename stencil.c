88e0a000cd57560e5be96996eb9bc53d82acbed5
Kwame Dogbe <kd16693@it052963.users.bris.ac.uk>
2018-10-16 17:59:59 +0100

    Changed arguements to static int variables

--- stencil.c
+++ stencil.c
@@ -50,13 +50,20 @@ int main(int argc, char *argv[]) {
 }
 
 void stencil(const int nx, const int ny, double *  image, double *  tmp_image) {
+
+  static int position, nyMult;
+
   for (int j = 0; j < ny; ++j) {
     for (int i = 0; i < nx; ++i) {
-      tmp_image[j+i*ny] = image[j+i*ny] * 0.6;
-      if (i > 0)    tmp_image[j+i*ny] += image[j  +(i-1)*ny] * 0.1;
-      if (i < nx-1) tmp_image[j+i*ny] += image[j  +(i+1)*ny] * 0.1;
-      if (j > 0)    tmp_image[j+i*ny] += image[j-1+i*ny] * 0.1;
-      if (j < ny-1) tmp_image[j+i*ny] += image[j+1+i*ny] * 0.1;
+
+      nyMult = i*ny;
+      position = j+nyMult;
+
+      tmp_image[position] = image[position] * 0.6;
+      if (i > 0)    tmp_image[position] += image[j   + nyMult - ny] * 0.1;
+      if (i < nx-1) tmp_image[position] += image[j   + nyMult + ny] * 0.1;
+      if (j > 0)    tmp_image[position] += image[j-1 + nyMult] * 0.1;
+      if (j < ny-1) tmp_image[position] += image[j+1 + nyMult] * 0.1;
     }
   }
 }
